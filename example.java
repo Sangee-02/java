2da807a8bcd2b4d788e58beecc80b50c45033dd8
Sangee-02 <sangeethapriyag2003@gmail.com>
2025-04-03 12:27:02 +0530

    sampleprogram.java

---
+++ example.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class mca_2024
+{
+void java()
+{
+System.out.println("Java_Application");
+}
+void softwareeng()
+{
+System.out.println("softwareeng");
+}
+void operatingsystem()
+{
+System.out.println("operating system");
+}
+
+public static void main(String[] args)
+{
+mca_2024 mca=new mca_2024();
+mca.java();
+mca.softwareeng();
+mca.operatingsystem();
+}
+}
